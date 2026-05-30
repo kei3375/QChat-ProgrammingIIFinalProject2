1d3957c17637ca22c25e928f84e356de367772f0
kenji3375 <lukborsuk@gmail.com>
2026-05-31 00:38:48 +0200

    tcp server and client template/example, showing time

---
+++ development/Luka/test_asio/tutorials/tcp_synchronous_daytime/server.cpp
@@ -0,0 +1,33 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <asio.hpp>
+
+using asio::ip::tcp;
+
+std::string make_daytime_string() {
+        std::time_t now = time(0);
+        return std::ctime(&now);
+}
+
+int main()
+{
+        try
+        {
+                asio::io_context io;
+
+                tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 81));
+
+                for(;;) {
+                        tcp::socket socket(io);
+                        acceptor.accept(socket);
+
+                        std::string message = make_daytime_string();
+                        std::error_code ignored_error;
+                        asio::write(socket, asio::buffer(message), ignored_error);
+                }
+        } catch (std::exception& e) {
+                std::cerr << e.what() << std::endl;
+        }
+        return 0;
+}
